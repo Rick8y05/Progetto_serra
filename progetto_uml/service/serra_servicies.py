0aa10a7d3495688d092b219e9be09e6ae51125ec
leonardobarelli <leonardo.barellilb3a@gmail.com>
2026-05-27 16:17:11 +0200

    Update serra_servicies.py

--- progetto_uml/service/serra_servicies.py
+++ progetto_uml/service/serra_servicies.py
@@ -18,9 +18,101 @@ class GestoreSerra:
         temperatura=self.serre_attive[n_univoco].get_temperatura_serra
         stato_ventole=self.serre_attive[n_univoco].get_stato_ventole_serra
         stato_impianto_irrigazione=self.serre_attive[n_univoco].get_stato_impianto_irrigazione
-        stato_lampadaUV=self.serre_attive[n_univoco].get_stato_lampadaUV
-        return [umidita,temperatura,stato_ventole,stato_impianto_irrigazione,stato_lampadaUV]
+        stato_lampada_uv=self.serre_attive[n_univoco].get_stato_lampadauv
+        return [umidita,temperatura,stato_ventole,stato_impianto_irrigazione,stato_lampada_uv]
     def verifica_esistenza_serra(self):#controlla che il codice univoco sia oresente
     #gestoreParcoSerra ricordarsi di mettere controolo con try except
+        pass
+
+
+
+class SerraService:
+
+    def __init__(self, path_serre, path_colture, path_temperature, path_umidita):
+
+        self.path_serre = path_serre
+        self.path_colture = path_colture
+        self.path_temperature = path_temperature
+        self.path_umidita = path_umidita
+
+        self.repo_serre = DatiRepository(self.path_serre, dict)
+        self.dati_serre = self.repo_serre.get_dati or {}
+
+        self.serre = {}
+
+        for codice, info in self.dati_serre.items():
+
+            codice = codice.strip().lower()
+
+            self.serre[codice] = Serra(
+                info.get("proprietario"),
+                info.get("coltura", "default"),
+                info.get("modalita", "manuale"),
+                self.path_temperature,
+                self.path_umidita,
+                codice
+            )
+
+    def get_serra(self, codice):
+        return self.serre.get(codice.strip().lower())
+    
+    def configura_coltura(self, codice_serra, coltura):
+        serra = self.get_serra(codice_serra)
+        if not serra:
+            print("Serra non trovata")
+            return
+        serra.coltura = coltura
+
+    def azione_manuale(self, codice, dispositivo, stato):
+
+        serra = self.get_serra(codice)
+
+        if not serra:
+            print("SERRA NON TROVATA")
+            return
+
+        stato_bool = stato.lower() == "on"
+
+        if dispositivo == "1":
+            serra.set_ventole(stato_bool)
+        elif dispositivo == "2":
+            serra.set_irrigazione(stato_bool)
+        elif dispositivo == "3":
+            serra.set_lampada_uv(stato_bool)
+        else:
+            print("Dispositivo non valido")
+
+        print("OK")
+
+    def set_modalita(self, modalita):
+        for s in self.serre.values():
+            s.modalita = "automatica" if modalita == "1" else "manuale"
+
+    def esegui_ciclo_automatico(self):
+        for s in self.serre.values():
+            s.mod_automatica()
+
+    def leggi_stato_serra(self, codice):
+        serra = self.get_serra(codice)
+
+        if not serra:
+            return None
+
+        return serra
+    
+    def get_dati_plancia(self):
+        serra = self.repository.get_serra_attiva()
+
+        if serra is None:
+            return None
+
+        return {
+            "ventole": serra.ventole,
+            "irrigazione": serra.irrigazione,
+            "lampada_uv": serra.lampada_uv,
+            "temperatura": serra.temperatura_serra,
+            "umidita": serra.umidita_serra,
+            "coltura": serra.coltura
+        }
 
 
