4d498bdf14036440e2a6b3736e8fc37f097d3907
leonardobarelli <leonardo.barellilb3a@gmail.com>
2026-06-02 14:25:26 +0200

    Update menu.py

--- progetto_uml/menu/menu.py
+++ progetto_uml/menu/menu.py
@@ -2,6 +2,9 @@ from services.autenticazione import Autenticazione
 
 class Menu:
 
+    domini = ["gmail.com", "outlook.com", "libero.it", "hotmail.com"]
+    caratteri_speciali = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", "|", ";", ":", "'", "\"", ",", ".", "<", ">", "/"]
+
     def __init__(self):
         self.auth = Autenticazione()
         self.running = True
@@ -15,7 +18,7 @@ class Menu:
     def scegli_opzione(self):
         return input("> ")
 
-    # ---------------- LOGIN ----------------
+    # login utente
     def accesso(self):
         email = input("Email: ").strip()
         password = input("Password: ").strip()
@@ -23,22 +26,55 @@ class Menu:
         utente = self.auth.login(email, password)
 
         if utente:
-            print(f"Login OK -> {utente.ruolo}")
+            print(f"Login OK")
+            print(f"Nome: {utente.nome}")
+            print(f"Ruolo: {utente.ruolo}")
             return utente
-        else:
+        else: 
             print("Login fallito")
             return None
+            
 
-    # ---------------- REGISTRAZIONE ----------------
+    # registrazione proprietario
     def registrazione(self):
-        email = input("Email: ")
-        password = input("Password (min 6): ")
+        nome = input("Nome: ").strip()
+        email = input("Email: ").strip()
+        password = input("Password (min 6): ").strip()
 
+    # ---------------- PASSWORD ----------------
         if len(password) < 6:
             print("Password troppo corta")
             return
 
-        if self.auth.register_proprietario(email, password):
+        ca_speciale = False
+        for car in password:
+            if car in self.caratteri_speciali:
+                ca_speciale = True
+                break
+
+        if not ca_speciale:
+            print("La password deve contenere almeno un carattere speciale!")
+            return
+
+    # ---------------- EMAIL ----------------
+        if "@" not in email:
+            print("Email non valida!")
+            return
+
+        dominio = email.split("@")[-1]
+
+        dominio_valido = False
+        for d in self.domini:
+            if dominio == d:
+                dominio_valido = True
+                break
+
+        if not dominio_valido:
+            print("Email non valida!")
+            return
+
+    # ---------------- REGISTRAZIONE ----------------
+        if self.auth.register_proprietario(nome, email, password):
             print("Registrazione OK")
         else:
             print("Utente già esistente")
