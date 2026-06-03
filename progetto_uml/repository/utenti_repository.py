9e7ffccd35b377ecebd004843ab7f8891a55a9d1
leonardobarelli <leonardo.barellilb3a@gmail.com>
2026-06-03 16:01:47 +0200

    Update utenti_repository.py

--- progetto_uml/repository/utenti_repository.py
+++ progetto_uml/repository/utenti_repository.py
@@ -8,6 +8,7 @@ class UtentiRepository:
             os.path.join(os.path.dirname(__file__), "..", "data", "utenti.json")
         )
 
+    # legge il file json
     def get_all(self):
         try:
             with open(self.path, "r", encoding="utf-8") as f:
@@ -15,9 +16,11 @@ class UtentiRepository:
         except:
             return []
 
+    # salva i dati nel file json
     def save_all(self, utenti):
         with open(self.path, "w", encoding="utf-8") as f:
             json.dump(utenti, f, indent=4, ensure_ascii=False)
 
+    # restituisce il percorso del file utenti.json
     def get_path(self):
         return self.path
