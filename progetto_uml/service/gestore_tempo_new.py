3370f8d1e19b02884c4b240e8da33e0a55d22aaf
leonardobarelli <leonardo.barellilb3a@gmail.com>
2026-06-04 10:08:35 +0200

    Refactor GestoreTempo class and improve comments

--- progetto_uml/service/gestore_tempo_new.py
+++ progetto_uml/service/gestore_tempo_new.py
@@ -1,7 +1,7 @@
 import threading
 import time
 
-# classe che gestisce la simulazione del tempo nel sistema esegue aggiornamenti periodici su sensori e serre tramite un thread separato
+# classe che gestisce la simulazione del tempo nel sistema
 class GestoreTempo:
     def __init__(self, gestore_serra, intervallo: float):
         self.gestore_serra = gestore_serra
@@ -10,24 +10,26 @@ class GestoreTempo:
         self.simulazione_attiva = False
         self.thread_tempo = None
 
+    # ciclo principale di simulazione temporale
     def loop_tempo(self):
-        #while non è invalidante gira separatamente da tutto il resto grazie a tred
+        #while non è invalidante, gira separatamente da tutto il resto grazie a tred
         while self.simulazione_attiva:
             time.sleep(self.intervallo)  # time slep non invalida perche gira separatamente da tutto il resto
             self.conta_cicli +=1
-            #effettua chiamata aggiornamento sensori ogni tot tempo
+            # effettua chiamata aggiornamento sensori ogni tot. tempo
             self.gestore_serra.simulazione_aggiornamento_sensori()
             self.gestore_serra.controllo_periodico()
 
-    def avvia(self):#chiamata per avviare il loop ad inzio simulazione
+    # chiamata per avviare il loop ad inzio simulazione
+    def avvia(self):
         if not self.simulazione_attiva:
             self.simulazione_attiva = True
             self.thread_tempo = threading.Thread(target=self.loop_tempo, daemon=True)
-            #la riga sopra con target serve a dire hai creato il ciclo di lavoro alternativo e deve fare self.loop_tempo, è un puntatore)
-            #daemon serve per dire se il main principale va in arresto fermati pure tredsecondario
-            #di default è su false e metterlo su true è una sicurezza in più anche se abbiamo fatto esci
+            # la riga sopra con target serve a dire "hai creato il ciclo di lavoro alternativo" e deve fare self.loop_tempo, è un puntatore)
+            # "daemon" serve per dire "se il main principale va in arresto, fermati"
+            # di default è su false e metterlo su true è una sicurezza in più anche se abbiamo fatto esci
             self.thread_tempo.start()
 
-
-    def ferma(self):#chiusura loop fine simulazione
+    # chiusura loop fine simulazione
+    def ferma(self):
             self.simulazione_attiva = False
