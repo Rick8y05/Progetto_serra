f2bc6be4121c9eb4267f4593f1c39cd7bb32f52c
leonardobarelli <leonardo.barellilb3a@gmail.com>
2026-06-19 19:51:18 +0200

    Update gestore_colture.py

--- progetto_uml/service/gestore_colture.py
+++ progetto_uml/service/gestore_colture.py
@@ -1,11 +1,11 @@
-#aggiornare parametri catalogo colture opeatore
-#configurare parametri colture
+# aggiornare parametri catalogo colture opeatore
+# configurare parametri colture
 from repository.lettore_dati import DatiRepository
 class GestoreColture:
     def __init__(self, dati_colture: dict , dati_colture_utente: dict ):
         self.dati_colture = dati_colture
         self.dati_colture_utente = dati_colture_utente
-    #sotto metodo per il cabio di un parametro di una coltura specifica
+    # sottometodo per il cabio di un parametro di una coltura specifica
     def aggiornamento_parametri_catalogo(self, pianta, tipo_parametro, nuovo_dato):
         try:
             controllo_valore = float(nuovo_dato)
@@ -25,12 +25,12 @@ class GestoreColture:
             else:
                 return False
 
-    #getter per vedere il catalogo aggiornato
+    # getter per vedere il catalogo aggiornato
     @property
     def get_catalogo_aggiornato(self):
         return self.dati_colture
-#metodo dove proprietario prende i dati dal softweare e li mette nella serra per mod automatica
 
+    # metodo in cui il proprietario prende i dati dal software e li inserisce nella serra per mod. automatica
     def configurazione_parametri_coltura(self,pianta: str):
         try:
             return self.dati_colture[pianta]
@@ -58,25 +58,5 @@ class GestoreColture:
             colture_disponibili.append(colture)
         return colture_disponibili
 
-
-
-
-
-
-#comunica con gestore serra, gestore serra chima configura_parametri_coltura che gli invia
-#solo i dati della pianta selezionata , ricordarsi che bisong afare in modo che mostra
-#il gestore serra che passi qui è quello istanziato ad inizio programma
-#ricordatelo perchè se metti un altro nome va in crash il programma
-
-
-
-
-
-
-
-
-
-
-
-
-
+# comunica con gestore serra, gestore serra chima configura_parametri_coltura che gli invia
+# solo i dati della pianta selezionata.
