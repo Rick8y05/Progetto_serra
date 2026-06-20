1d4f68792cb7d1a07884c3937173db067edca988
AleMeno05 <alemeno2005@gmail.com>
2026-06-20 11:13:15 +0200

    Update sensore.py

--- progetto_uml/models/sensore.py
+++ progetto_uml/models/sensore.py
@@ -1,30 +1,40 @@
 from repository.lettore_dati import DatiRepository
 
-# definizione della classe Sensori che rappresenta un sensore della serra che legge valori da una lista di dati
+
 class Sensori:
+    """
+    SIMULAZIONE SENSORI
+    """
     def __init__(self, tipo: str, dati):
+        """
+        Inizializzazione sensori
+        """
         self._tipo = tipo
         self.dati = dati
         self.indice_corrente = 0
         self.valore_attuale = self.dati[self.indice_corrente]
 
-    # restituisce il valore attualmente rilevato dal sensore
+    
     def get_dati(self):
+        """
+        Restituisce il valore attualmente rilevato dal sensore
+        """
         return self.valore_attuale
 
     # property per ottenere il tipo del sensore
     @property
-    def tipo(self) -> str:
+    def tipo(self) -> str
+    """
+    Metodo per ottenere il tipo del sensore
+    """
         return self._tipo
 
-    # aggiorna il sensore
+    
     def aggiornamento_sensore(self):
-        # Incrementa l'indice
+        """
+        Aggiorna il valore corrente del sensore avanzando nell'indice dei dati
+        """
         self.indice_corrente += 1
-
-        # Controllo di sicurezza: se esce dal range, resetta a zero
         if self.indice_corrente >= len(self.dati):
             self.indice_corrente = 0
-
-        # Aggiorna il valore attuale
         self.valore_attuale = self.dati[self.indice_corrente]
