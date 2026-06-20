d737e5c7365bc6efbd3ea2716f81e3f55d32687f
AleMeno05 <alemeno2005@gmail.com>
2026-06-20 09:46:11 +0200

    Update gestore_colture.py

--- progetto_uml/service/gestore_colture.py
+++ progetto_uml/service/gestore_colture.py
@@ -1,12 +1,18 @@
-# aggiornare parametri catalogo colture opeatore
-# configurare parametri colture
 from repository.lettore_dati import DatiRepository
+
 class GestoreColture:
     def __init__(self, dati_colture: dict , dati_colture_utente: dict ):
+        """
+        INIZIALIZZAZIONE GESTORE COLTURE
+        """
         self.dati_colture = dati_colture
         self.dati_colture_utente = dati_colture_utente
-    # sottometodo per il cabio di un parametro di una coltura specifica
+        
+    
     def aggiornamento_parametri_catalogo(self, pianta, tipo_parametro, nuovo_dato):
+        """
+        Metodo per il cabio di un parametro di una coltura specifica
+        """
         try:
             controllo_valore = float(nuovo_dato)
         except (ValueError, TypeError):
@@ -25,19 +31,30 @@ class GestoreColture:
             else:
                 return False
 
-    # getter per vedere il catalogo aggiornato
+   
     @property
     def get_catalogo_aggiornato(self):
+        """
+        Getter per vedere il catalogo aggiornato
+        """
         return self.dati_colture
 
-    # metodo in cui il proprietario prende i dati dal software e li inserisce nella serra per mod. automatica
+    
     def configurazione_parametri_coltura(self,pianta: str):
+        """
+        Metodo in cui il proprietario prende i dati dal software e li inserisce nella serra per mod. automatica
+        """
         try:
             return self.dati_colture[pianta]
         except KeyError:
             print(f"la pianta {pianta} non è presente nel catalogo")
             return None
+
+    
     def caricamento_dati_fenotipici(self,pianta:str, proprietario: str, busto: float, altezza: float, impostazioni: bool):
+        """
+        Metodo con cui il proprietario inserisci i nuovi dati fenotipici per la pianta 
+        """
         if pianta in self.dati_colture:
             dati_nuovi= {"pianta": pianta,
                          "busto": busto,
@@ -48,15 +65,21 @@ class GestoreColture:
         else:
             print("pianta non prensete nel catalogo")
 
+    
     def rimuovi_dati_utente(self, proprietario: str):
+        """
+        Metodo per la rimozione dei dati caricati da un proprietario nell file dati colture utente 
+        """
         if proprietario in self.dati_colture_utente:
             self.dati_colture_utente.pop(proprietario)
 
+    
     def colture_disponibili (self):
+        """
+        Metodo che restituisce le colture disponibili 
+        """
         colture_disponibili = []
         for colture in self.dati_colture:
             colture_disponibili.append(colture)
         return colture_disponibili
 
-# comunica con gestore serra, gestore serra chima configura_parametri_coltura che gli invia
-# solo i dati della pianta selezionata.
