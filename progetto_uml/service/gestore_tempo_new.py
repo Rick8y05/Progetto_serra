70defb2e4a4065cbbb67971cbba9a4401c0b2fc3
AleMeno05 <alemeno2005@gmail.com>
2026-06-20 09:38:49 +0200

    Update gestore_tempo_new.py

--- progetto_uml/service/gestore_tempo_new.py
+++ progetto_uml/service/gestore_tempo_new.py
@@ -4,14 +4,20 @@ import time
 # classe che gestisce la simulazione del tempo nel sistema
 class GestoreTempo:
     def __init__(self, gestore_serra, intervallo: float):
+        """
+        INIZIALIZZAZIONE GESTORE TEMPO
+        """
         self.gestore_serra = gestore_serra
         self.intervallo = intervallo
         self.conta_cicli = 0
         self.simulazione_attiva = False
         self.thread_tempo = None
 
-    # ciclo principale di simulazione temporale
+    
     def loop_tempo(self):
+        """
+        Ciclo principale di simulazione temporale
+        """
         # while non è invalidante, gira separatamente da tutto il resto grazie a "tred"
         while self.simulazione_attiva:
             time.sleep(self.intervallo)  
@@ -20,13 +26,19 @@ class GestoreTempo:
             self.gestore_serra.simulazione_aggiornamento_sensori()
             self.gestore_serra.controllo_periodico()
 
-    # chiamata per avviare il loop ad inzio simulazione
+   
     def avvia(self):
+        """
+        chiamata per avviare il loop ad inzio simulazione
+        """
         if not self.simulazione_attiva:
             self.simulazione_attiva = True
             self.thread_tempo = threading.Thread(target=self.loop_tempo, daemon=True)
             self.thread_tempo.start()
 
-    # chiusura loop fine simulazione
+   
     def ferma(self):
+        """
+        Chiusura loop fine simulazione
+        """
             self.simulazione_attiva = False
