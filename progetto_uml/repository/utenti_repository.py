6122b24a88c61b4c360d24630c0c34cb1d272f58
AleMeno05 <alemeno2005@gmail.com>
2026-06-20 10:35:01 +0200

    Update utenti_repository.py

--- progetto_uml/repository/utenti_repository.py
+++ progetto_uml/repository/utenti_repository.py
@@ -1,9 +1,11 @@
 import json
 import os
 
-# questa classe si occupa della gestione del file utenti.json, leggendo e scrivendo la lista degli utenti
-class UtentiRepository:
 
+class UtentiRepository:
+"""
+Classe dedicata alla persistenza dei dati utente su file JSON
+"""
     def __init__(self):
         """
         INIZIALIZZAZIONE CLASSE 
@@ -13,20 +15,28 @@ class UtentiRepository:
             os.path.join(os.path.dirname(__file__), "..", "data", "utenti.json")
         )
 
-    # legge il file json con gli utenti
+    
     def get_all(self):
+        """
+        Legge il file json con gli utenti
+        """
         try:
             with open(self.path, "r", encoding="utf-8") as f:
                 return json.load(f)
-        # in caso di errore restituisce una lista vuota
         except:
             return []
 
-    # salva i dati degli utenti nel file json
+   
     def save_all(self, utenti):
+        """
+        Salva i dati degli utenti nel file json
+        """
         with open(self.path, "w", encoding="utf-8") as f:
             json.dump(utenti, f, indent=4, ensure_ascii=False)
 
-    # restituisce il percorso del file utenti.json
+
     def get_path(self):
+        """
+        Restituisce il percorso del file utenti.json
+        """
         return self.path
